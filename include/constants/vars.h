8f7d0a7f837ce6dd6a6d655462e17108296883a6
HashtagMarky <143505183+HashtagMarky@users.noreply.github.com>
2026-07-15 14:29:36 +0100

    Very Loose Scripting

--- include/constants/vars.h
+++ include/constants/vars.h
@@ -266,7 +266,7 @@
 #define VAR_ROXANNE_CALL_STEP_COUNTER                    0x40F4
 #define VAR_SCOTT_BF_CALL_STEP_COUNTER                   0x40F5
 #define VAR_RIVAL_RAYQUAZA_CALL_STEP_COUNTER             0x40F6
-#define VAR_UNUSED_0x40F7                                0x40F7 // Unused Var
+#define VAR_GAME_STATE                                   0x40F7 // Unused Var
 #define VAR_UNUSED_0x40F8                                0x40F8 // Unused Var
 #define VAR_UNUSED_0x40F9                                0x40F9 // Unused Var
 #define VAR_UNUSED_0x40FA                                0x40FA // Unused Var
@@ -337,5 +337,12 @@
 #define TESTING_VAR_UNUSED_6                (TESTING_VARS_START + 0x6)
 #define TESTING_VAR_UNUSED_7                (TESTING_VARS_START + 0x7)
 #endif // TESTING
+
+enum GameStates
+{
+    GS_INTRO,
+    GS_WOKEN_UP,
+    GS_FOREST_ROAD,
+};
 
 #endif // GUARD_CONSTANTS_VARS_H
