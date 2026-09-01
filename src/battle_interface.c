ca7c150c9f687c5655ad9b07657966ca7c51c9a4
HashtagMarky <143505183+HashtagMarky@users.noreply.github.com>
2026-09-01 21:20:58 +0100

    Switch Opponents Position When Double

--- src/battle_interface.c
+++ src/battle_interface.c
@@ -837,8 +837,8 @@ static const s16 sBattlerHealthboxCoords[BATTLE_COORDS_COUNT][MAX_BATTLERS_COUNT
     {
         [B_POSITION_PLAYER_LEFT]    = {  24, 42 },
         [B_POSITION_PLAYER_RIGHT]   = { 170, 42 },
-        [B_POSITION_OPPONENT_LEFT]  = {  44, 16 },
-        [B_POSITION_OPPONENT_RIGHT] = { 170, 16 },
+        [B_POSITION_OPPONENT_LEFT]  = { 170, 16 },
+        [B_POSITION_OPPONENT_RIGHT] = {  44, 16 },
     },
 };
 
