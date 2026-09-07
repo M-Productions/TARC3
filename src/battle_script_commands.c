f4d694aa4343a543e1c926ff6f54b93b82279002
HashtagMarky <143505183+HashtagMarky@users.noreply.github.com>
2026-09-07 07:10:52 +0100

    Disable XP

--- src/battle_script_commands.c
+++ src/battle_script_commands.c
@@ -3811,6 +3811,7 @@ FEATURE_FLAG_ASSERT(I_EXP_SHARE_FLAG, YouNeedToSetTheExpShareFlagToAnUnusedFlag)
 
 static bool32 BattleTypeAllowsExp(void)
 {
+    return FALSE;
     if (RECORDED_WILD_BATTLE)
         return TRUE;
     else if (gBattleTypeFlags &
