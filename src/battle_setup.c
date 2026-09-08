5821d2b02ab1fb42673a326c804e1cb46d0f8f93
HashtagMarky <143505183+HashtagMarky@users.noreply.github.com>
2026-09-08 13:44:18 +0100

    Buff battles

--- src/battle_setup.c
+++ src/battle_setup.c
@@ -1277,9 +1277,9 @@ void StartPuzzleBattleScript(struct ScriptContext *ctx)
 void StartNonPuzzleBattle(void)
 {
     HealPlayerParty();
-    for (s32 i = 1; i < PARTY_SIZE; i++)
-        ZeroMonData(&gParties[B_TRAINER_PLAYER][i]);
-    gPartiesCount[B_TRAINER_PLAYER] = 1;
+    // for (s32 i = 1; i < PARTY_SIZE; i++)
+    //     ZeroMonData(&gParties[B_TRAINER_PLAYER][i]);
+    // gPartiesCount[B_TRAINER_PLAYER] = 1;
     ZeroEnemyPartyMons();
     struct Pokemon *player = &gParties[B_TRAINER_PLAYER][0];
     const struct BattlePuzzle *puzzlesData = &sBattlePuzzles[BP_HEADBUTT];
